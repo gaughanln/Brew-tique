924b5cd5caa9a75c0f2dff7031e31192c9c6d50b
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-05-04 19:04:21 -0600

    added coffee data - seeds.js, coffeeData.js, and Coffee.js model

---
+++ server/seeds/coffeeData.js
@@ -0,0 +1,25 @@
+const coffees = [
+  {
+    name: 'Ethiopian Yirgacheffe',
+    location: 'Ethiopia',
+    roast: 'Light',
+    description: 'A bright and floral coffee with a delicate tea-like body.',
+    price: 14.99,
+  },
+  {
+    name: 'Colombian Supremo',
+    location: 'Colombia',
+    roast: 'Medium',
+    description: 'A rich, full-bodied coffee with a sweet and fruity aroma.',
+    price: 12.99,
+  },
+  {
+    name: 'Sumatra Mandheling',
+    location: 'Indonesia',
+    roast: 'Dark',
+    description: 'A bold and earthy coffee with a heavy body and low acidity.',
+    price: 13.99,
+  },
+];
+
+module.exports = coffees;
