3693f5192c2796437ff7c70cf630ab346fbea7f5
gaughanln <gaughanln@yahoo.com>
2023-05-12 13:25:03 -0500

    added some styling and buttons

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import oops from "../assets/oops.png";
+import carttext from '../assets/carttext.png'
 
 class Cart extends React.Component {
   constructor(props) {
@@ -10,25 +13,65 @@ class Cart extends React.Component {
 
   render() {
     // const { cartItems } = this.state;
-    const { cart: cartItems} = this.props; // the props is taking the item from the shop menu and lifting it to the parent and then sending it to the cart
+    const { cart: cartItems } = this.props; // the props is taking the item from the shop menu and lifting it to the parent and then sending it to the cart
     return (
       <div>
-        <h1>Cart</h1>
         {cartItems.length === 0 ? (
-          <div>Your cart is empty.</div>
+          <>
+            <div className="row  valign-wrapper ">
+              <div class="col center-align ">
+                <img src={oops} className="oops" alt="Your cart is empty" />
+                <br />
+                <Link
+                  className="btn-large waves-effect  green-btn"
+                  to="/products"
+                >
+                  Shop now!
+                </Link>
+              </div>
+            </div>
+          </>
         ) : (
           <div>
-            <ul>
+           <img src={carttext} className="cart-header" alt="You've got great taste" />
+            <div className="row cards valign-wrapper center-align">
               {cartItems.map((item, index) => (
-                <li key={index}>
-                  {item.name} - {item.price}
-                </li>
+                <a key={index}>
+                  <div class="col s6 center-align">
+                    {/*  image */}
+                    <img
+                      src={item.image}
+                      className="inthecart"
+                      height="200px"
+                    />
+                    <br />
+                  </div>
+
+                  {/* name and price */}
+                  <div class="col s6 center-align">
+                    <p className="cart-text">
+                      {item.name} <br /> $ {item.price}
+                    </p>
+                  </div>
+                </a>
               ))}
-            </ul>
-            <div>
-              Total: {cartItems.reduce((acc, item) => acc + item.price, 0)}
             </div>
-            <button>Checkout</button>
+            <div>
+              <div className="row cards center-align">
+                <div class="col s6 center-align waves-effect  btn-large checkout-btn ">
+                  {/* total */}
+                  <strong>
+                    Total: ${" "}
+                    {cartItems.reduce((acc, item) => acc + item.price, 0)}
+                  </strong>
+                </div>
+
+                {/* submit button */}
+                <button className="waves-effect btn-large brown-btn checkout-btn ">
+                  Checkout
+                </button>
+              </div>
+            </div>
           </div>
         )}
       </div>
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,69 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import ontap from "../assets/ontap.png"
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import ontap from "../assets/ontap.png";
+import dreams from "../assets/dreams.png";
+import sundown from "../assets/sundown.png";
+
+import medium from "../assets/medium.png";
 
 // from stevie
 // import { useQuery } from '@apollo/react-hooks';
 // import { GET_PRODUCTS } from '../../utils/queries';
 
 const getProducts = () => {
-  return ({
-    data:[
-    {
-      id: 1,
-      name: 'Light Roast',
-      price: 10.99,
-      image: '',
-    },
-    {
-      id: 2,
-      name: 'Medium Roast',
-      price: 10.99,
-      image: '',
-    },
-    {
-      id: 3,
-      name: 'Ultra Premium "you need this in your life" Dark Roast',
-      price: 30.99,
-      image: '',
-    }
-  ], loading: false})
-}
-
-
+  return {
+    data: [
+      {
+        id: 1,
+        name: "Light Roast",
+        price: 10.99,
+        image: dreams,
+      },
+      {
+        id: 2,
+        name: "Medium Roast",
+        price: 10.99,
+        image: sundown,
+      },
+      {
+        id: 3,
+        name: "Ultra Premium",
+        price: 30.99,
+        image: medium,
+      },
+    ],
+    loading: false,
+  };
+};
 
 const Products = (props) => {
-const {cart, setCart} = props;
-const {data, loading} = getProducts(); //replace this with the graphQl query
-const [products, setProducts] = useState([])
- useEffect(() => {
-  if (!loading) {
-    setProducts(data)
-  }})
-
+  const { cart, setCart } = props;
+  const { data, loading } = getProducts(); //replace this with the graphQl query
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    if (!loading) {
+      setProducts(data);
+    }
+  }, [data, loading]);
 
   const addToCart = (product) => {
     setCart([...cart, product]);
-  }
-
-
-
+  };
 
   return (
     <div>
       {/* <h2>Products</h2> */}
-
-      <img src ={ontap} className="ontap" alt="Products" />
-
-      <div style={{display:"flex"}}>
-        {products.map((product) => (
-          <div key={product.id} style={{border: "1px solid black", margin: "0px 14px"}}>
-            <img src={product.image} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-            <button className="waves-effect  btn-large brown-btn" onClick={() => addToCart(product)}>Add to Cart</button>
-          </div>
-        ))}
-     </div>
+      <div className="container  center-align">
+        <img src={ontap} className="ontap" alt="Products" />
+        <div className="row">
+        <div className="products-text" >
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="products col s12 m6 l4"
+              
+            >
+              <img
+                src={product.image}
+                className="products-img"
+                alt={product.name}
+                height="300"
+              />
+              <h3>{product.name}</h3>
+              <p>${product.price}</p>
+              <button
+                className="waves-effect  btn-large brown-btn"
+                onClick={() => addToCart(product)}
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
+        </div>
+        <Link
+            className="btn-large waves-effect  green-btn" to="/cart" > Checkout </Link>
+      </div>
     </div>
   );
 };
