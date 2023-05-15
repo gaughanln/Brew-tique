d1e133ad5ebf4c92babd285fdf41ad7a1b791724
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-14 20:35:37 -0500

    addition by subtraction

--- server/server.js
+++ server/server.js
@@ -32,18 +32,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  });
 
-  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-  
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+});
 
-  startApolloServer();
-  
+
+startApolloServer();
+
