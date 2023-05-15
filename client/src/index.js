cfbfda647de2dd3fd37596d92ca84196bc8eb98a
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-14 20:33:02 -0500

    oops

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,6 @@ import App from "./App";
 import client from './apolloClient'; 
 import { ApolloProvider } from '@apollo/client';
 
-if(process.env.NODE_ENV !== 'production') {
-  db.connect(process.env.MONGODB_URI);
-} else {
-  db.connect('mongodb://127.0.0.1:27017/Brewtique');
-}
-
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
