8890b97c5654bc90af1b93b61b70f07880900c12
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-14 22:40:56 -0500

    middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,34 +1,33 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({ path: '../.env' });
 
 const secret = process.env.REACT_APP_JWT_SECRET;
 const expiration = '2h';
 
-function authMiddleware({ req })  {
-  let token = req.body.token || req.query.token || req.headers.authorization;
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-  if (req.headers.authorization) {
-    token = token.split(' ').pop().trim();
-  }
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-  if (!token) {
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
     return req;
-  }
+  },
 
-  try {
-    const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    req.user = data;
-  } catch {
-    console.log('Invalid token');
-  }
-  return req;
-};
+  signToken: function ({ email, firstName, lastName, _id }) {
+    const payload = { email, firstName, lastName, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+}
 
-
-
-function signToken ({ email, firstName, lastName, _id }) {
-  const payload = { email, firstName, lastName, _id };
-  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-};
-
-module.exports = { authMiddleware, signToken };
