0a25ca87591dd77379bbac2eb5c8e8042e0fbda9
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-15 01:00:47 -0500

    production

--- server/server.js
+++ server/server.js
@@ -28,8 +28,11 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
 }
   
   db.once('open', () => {
