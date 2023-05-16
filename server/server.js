d313f3c2210676904eccab8a85c5a6fe0b8e4d83
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-15 19:22:40 -0500

    server build?

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.static("client/build"));
 
 if (process.env.NODE_ENV === "production") {
   app.get('/*', function (req, res) {
-    const index = path.join(__dirname, '..client/build/', 'index.html');
+    const index = path.join(__dirname, 'index.html');
     res.sendFile(index);
   });
 }
